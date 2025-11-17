95261c9174c2118521c1bbed8453713278bb3db5
strom314 <jakub.gros777@gmail.com>
2025-11-17 15:03:54 +0100

    create an input state for cards and make the buttons increment and decrement it's value

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,32 @@
-export default function Card() {
+import { useState } from "react";
+
+export default function Card({ imgSrc, title, rating, price }) {
+  const [amount, setAmount] = useState(1);
+
+  function handleInput(e) {
+    setAmount(e.target.value);
+  }
+  function handlePlus() {
+    setAmount(Number(amount) + 1);
+  }
+  function handleMinus() {
+    if (amount > 0) {
+      setAmount(Number(amount) - 1);
+    }
+  }
+
   return (
     <div>
+      <img src={imgSrc} alt={title + " image"} />
       <ul>
-        <li>title</li>
-        <li>rating</li>
-        <li>price</li>
+        <li>{title}</li>
+        <li>{rating}</li>
+        <li>{price}</li>
       </ul>
 
-      <button>-</button>
-      <input type="number" />
-      <button>+</button>
+      <button onClick={handleMinus}>-</button>
+      <input type="number" value={amount} onInput={handleInput} />
+      <button onClick={handlePlus}>+</button>
       <button>add to cart</button>
     </div>
   );
