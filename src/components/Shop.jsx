8b757256d1f7e2971b5434704c9b3e72e96f9ca5
strom314 <jakub.gros777@gmail.com>
2025-11-17 15:16:55 +0100

    create a useEffect that fetches data from fakestore api and then creates cards on tha page with the fetched data

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,11 +1,32 @@
 import Card from "./Card";
+import { useEffect, useState } from "react";
 
 export default function Shop() {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+      });
+  }, []);
+
   return (
     <>
       <h1>Shop</h1>
-      <Card />
-      <Card />
+      {products &&
+        products.map((product) => {
+          return (
+            <Card
+              imgSrc={product.image}
+              title={product.title}
+              rating={product.rating.rate}
+              price={product.price}
+            />
+          );
+        })}
     </>
   );
 }
