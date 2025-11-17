48402a9781065b179e659c570f742542453aa66f
strom314 <jakub.gros777@gmail.com>
2025-11-17 11:47:39 +0100

    create a router and make it route between the components

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,9 +1,32 @@
+import { Children } from "react";
 import App from "./components/App";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
 
 const routes = [
   {
     path: "/",
     element: <App />,
+
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "shop",
+        element: <Shop />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+    ],
   },
 ];
 
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
-
+import { Link, Outlet } from "react-router";
 
 function App() {
-
   return (
     <>
-      
+      <div>
+        <Link to={"/home"}>home</Link>
+        <Link to={"/shop"}>shop</Link>
+      </div>
+      <Link to={"/cart"}>cart</Link>
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
