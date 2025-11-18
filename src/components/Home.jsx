985c77130b7e7c47b87738d094adc97c820285a4
strom314 <jakub.gros777@gmail.com>
2025-11-18 20:42:26 +0100

    style home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,10 @@
+import styles from "./home.module.css";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className={styles.content}>
+      <h1>Home</h1>
+      <p>This is an awesome shop. You can buy many things here :)</p>
+    </div>
+  );
 }
